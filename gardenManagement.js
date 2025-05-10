3842f6d14e4cb38795e0723c90d7d07360498ef1
Ridge <ridge.telegrafi@icloud.com>
2025-05-10 01:06:46 -0400

    automate garden watering, lighting, and soil moisture updates

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,17 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+//Watering Control
+if (temperature>80) {console.log("Watering on");}
+else {console.log("Watering off");}
+//Garden Lights Control
+if (timeOfDay=== "evening"|"night"){console.log("Lights on.");}
+else {console.log("Lights off")}
+//Soil Moisture Adjustment
+
+while (soilMoisture<41){
+    console.log(soilMoisture);
+    soilMoisture+=5;
+}
